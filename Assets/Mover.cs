9dbd39c36bdf6fa5a15c5c6b3aec5df7c4c3be2a
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-05-10 21:17:19 -0400

    Remove history from Mover script

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // ~~~~~~~~~~ For moving forward then back experimentation ~~~~~~~~~~
-    // float zPositionTracker = 0f;
-    // float zPositionLimiter = 10f;
-    // float xPositionCrementer = 0f;
-    // float yPositionCrementer = 0f;
-    // float zPositionCrementer = 0.01f;
-    // bool isRunningAway = true;
-    // bool isRunning = true;
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
     // Start is called before the first frame update
     void Start()
     {
@@ -26,25 +16,5 @@ public class Mover : MonoBehaviour
         float xPositionCrementer = unitsToMovePerSecond * Input.GetAxis("Horizontal") * Time.deltaTime;
         float zPositionCrementer = unitsToMovePerSecond * Input.GetAxis("Vertical") * Time.deltaTime;
         transform.Translate(xPositionCrementer, 0, zPositionCrementer);
-        // ~~~~~~~~~~~ Moving forward and back ~~~~~~~~~~~~~~~~~~~~~~~~~
-        // if (isRunningAway) {
-        //     zPositionCrementer = 0.01f;
-        // }
-        // else {
-        //     zPositionCrementer = -0.01f;
-        // }
-        // zPositionTracker += zPositionCrementer;
-        // Debug.Log("zPositionTracker: " + zPositionTracker);
-        // Debug.Log("isRunningAway: " + isRunningAway);
-        // if (zPositionTracker > zPositionLimiter) {
-        //     isRunningAway = false;
-        // }
-        // else if (zPositionTracker < 0) {
-        //     isRunning = false;
-        // }
-        // if (isRunning) {
-        //     transform.Translate(xPositionCrementer, yPositionCrementer, zPositionCrementer);
-        // }
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     }
 }
